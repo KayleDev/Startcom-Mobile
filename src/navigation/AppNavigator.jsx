cc41c7318d3a9dbb89b43a750bfb245952ab112f
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-10-07 09:25:53 -0300

    Feature: Navigation between screens

---
+++ src/navigation/AppNavigator.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+// Screens
+import Login from "../screens/Auth/Login/";
+import Register from "../screens/Auth/Register/";
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {/* Public Screens */}
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
