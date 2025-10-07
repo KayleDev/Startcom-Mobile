6669bbeaf31e50d90112ab4bbc7b476655c87d4f
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-10-07 12:07:15 -0300

    Style: Initial color variables in global styles

---
+++ src/styles/globalStyle.js
@@ -0,0 +1,11 @@
+export const globalStyle = {
+  primary: '#017688',
+  secondary: '#e4e8ef',
+  tertiary: '#487281',
+  quaternary: '#373f51',
+  quinary: '#a9bcd0',
+  senary: '#282828',
+
+  componentBackgroundColor: '#F1F1F1',
+  primaryLayoutBackgroundColor: '#0D0A0A',
+}
