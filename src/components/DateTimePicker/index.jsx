68cca5303a57a91e3d95b50bac8558e3fc1fa72a
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-10-11 20:55:28 -0300

    Style: DateTimePicker component

---
+++ src/components/DateTimePicker/index.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { View, Text, TouchableOpacity, Platform } from 'react-native';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { styles } from '../Input/styles.js';
+import { styles as datePickerStyles } from './styles.js';
+
+export default function DatePickerInput({ date, setDate, label }) {
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    setShow(false);
+    if (selectedDate) setDate(selectedDate);
+  };
+
+  return (
+    <View style={{ width: "49%", marginBottom: 16 }}>
+      {label && <Text style={styles.labelText}>{label}</Text>}
+      
+      <TouchableOpacity
+        onPress={() => setShow(true)}
+        style={datePickerStyles.dateInputContainer}
+      >
+        <Text style={datePickerStyles.dateText}>
+          {date.toLocaleDateString('pt-BR')}
+        </Text>
+        <Text style={{ fontSize: 18, color: '#017688' }}>📅</Text>
+
+        {show && (
+          <DateTimePicker
+            value={date}
+            mode="date"
+            display={Platform.OS === 'ios' ? 'spinner' : 'calendar'}
+            onChange={onChange}
+          />
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ src/components/DateTimePicker/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+import { globalStyle } from "../../styles/globalStyle.js";
+
+export const styles = StyleSheet.create({
+    dateInputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        backgroundColor: globalStyle.secondary,
+        color: '#333',
+        borderRadius: 8,
+        paddingHorizontal: 12,
+        height: 48,
+    },
+
+    dateText: {
+        fontSize: 16,
+        color: globalStyle.quinary
+    },
+});
