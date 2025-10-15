c86adba776a7c3a99209a6047ea4221bbd2df10f
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-10-14 23:42:04 -0300

    Style: Sales screen.

--- src/screens/Sales/index.jsx
+++ src/screens/Sales/index.jsx
@@ -1,17 +1,32 @@
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, ScrollView, Text } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../../layout/Header';
 import Sidebar from '../../layout/Sidebar';
+import Button from '../../components/Button';
 
 import { styles } from './styles';
 import { commonUserStyles } from '../../styles/commonUserStyles.js';
+import SalesCard from '../../components/SalesCard';
+import Input from '../../components/Input';
+import PeriodSelector from '../../components/PeriodSelector';
+import StatusFilter from '../../components/StatusFilter';
+
+import { Plus, DollarSign, ShoppingCart, TrendingUp} from 'lucide-react-native';
+import { globalStyle } from '../../styles/globalStyle.js';
+import SalesInfo from '../../components/SalesInfo/index.jsx';
 
 const Sales = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   const navigation = useNavigation();
   const route = useRoute();
+
+  const [search, setSearch] = useState("")
+
+  const handleSale = () => {
+    console.log("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+  }
 
   return (
     <SafeAreaView style={commonUserStyles.safeArea}>
@@ -20,7 +35,7 @@ const Sales = () => {
         title="Vendas"
       />
 
-      <View style={commonUserStyles.screenBlock}>
+      <ScrollView style={commonUserStyles.screenBlock}>
         <Text style={commonUserStyles.screenTitle}>
           Vendas
         </Text>
@@ -28,7 +43,71 @@ const Sales = () => {
         <Text style={commonUserStyles.screenDescription}>
           Gerencie todas as suas vendas e transações
         </Text>
-      </View>
+
+        <Button 
+          title={
+            <View style={commonUserStyles.alignButtonText}>
+              <Plus size={20} color="#FFFFFF" />
+              <Text style={commonUserStyles.saveText}>Nova Venda</Text>
+            </View>          
+          }
+          onPress={handleSale}
+        />
+
+
+        <View style={styles.salesContainer}>
+          <SalesCard 
+            title="Vendas Hoje"
+            amount="R$ 1.247,50"
+            valuePlus="+12.5% vs ontem"
+            icon={<DollarSign color={globalStyle.primary}/>}
+            iconBgColor={globalStyle.primaryTransparent}
+          />
+
+          <SalesCard 
+            title="Total de Vendas"
+            amount="156"
+            valuePlus="+8 esta semana"
+            icon={<ShoppingCart color={globalStyle.primary}/>}
+            iconBgColor={globalStyle.primaryTransparent}
+          />
+
+          <SalesCard 
+            title="Ticket Médio"
+            amount="R$ 89,50"
+            valuePlus="+5.2% este mês"
+            icon={<TrendingUp color={globalStyle.primary}/>}
+            iconBgColor={globalStyle.primaryTransparent}
+          />
+        </View>
+
+        <View style={styles.filterContainer}>
+          <Input 
+            placeholder="Buscar Vendas por cliente, ID..." 
+            styleInput={styles.inputVariant}
+            onChangeText={setSearch}
+            value={search}
+          />
+
+          <View style={styles.filters}>
+            <PeriodSelector
+              periods={["Hoje", "Esta Semana", "Este Mês", "Este Ano"]}
+              defaultPeriod="Este Mês"
+              containerStyle={{width: "49%"}}
+            />
+
+            <StatusFilter
+              onFilterChange={(selectedFilters) => {
+                console.log('Filtros selecionados:', selectedFilters);
+              }}
+              containerStyle={{width: "49%"}}
+            />
+          </View>
+        </View>
+
+        <SalesInfo/>
+        
+      </ScrollView>
 
       <Sidebar
         isOpen={isSidebarOpen}
---
+++ src/screens/Sales/styles.js
@@ -0,0 +1,37 @@
+import { StyleSheet } from "react-native";
+import { globalStyle } from "../../styles/globalStyle";
+
+export const styles = StyleSheet.create({
+    salesContainer: {
+        gap: 10,
+        marginVertical: 5,
+    },
+
+    filterContainer: {
+        gap: 2.5,
+        marginVertical: 20,
+        padding: 20,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.08,
+        shadowRadius: 8,
+        elevation: 3,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 10,
+        backgroundColor: '#fff',
+    },
+
+    inputVariant: {
+        backgroundColor: "#FFFFFF", 
+        borderWidth: 1, 
+        borderColor: '#ccc',
+        borderRadius: 10,
+    },
+
+    filters: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    }
+})
