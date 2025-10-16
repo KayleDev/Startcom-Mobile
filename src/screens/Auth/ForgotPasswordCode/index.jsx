efe1f578c224fc178caa03873b878c733e1db113
Caio Alessandro <142769379+KayleDev@users.noreply.github.com>
2025-10-16 09:46:36 -0300

    Feat: ForgotPasswordCode screen

---
+++ src/screens/Auth/ForgotPasswordCode/index.jsx
@@ -0,0 +1,47 @@
+import { Text, View, Alert } from "react-native"
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
+import Button from '../../../components/Button';
+import BackButton from "../../../components/BackButton";
+import CodeVerificator from "../../../components/CodeVerificator";
+
+import { styles } from "./styles";
+
+const ForgotPasswordCode = () => {
+    const navigation = useNavigation();
+    const [verifiedCode, setVerifiedCode] = useState('');
+
+    const handleCodeComplete = (fullCode) => {
+        setVerifiedCode(fullCode);
+        // Back-end validation is here. - Caio
+        navigation.navigate('ChangePassword');
+    }
+
+    const handleRedirect = () => {
+        if (!verifiedCode || verifiedCode.length !== 6) {
+            Alert.alert("Erro", "Por favor, insira os 6 dígitos do código!");
+            return;
+        }
+        navigation.navigate('ChangePassword')
+    }
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <BackButton/>
+            <Text style={styles.title}>Redefinição de senha</Text>
+            <Text style={styles.description}>Insira o código enviado por e-mail.</Text>
+
+            <View style={styles.actions}>
+                <CodeVerificator onCodeComplete={handleCodeComplete}/>
+                <Button         
+                    title="Enviar" 
+                    onPress={handleRedirect} 
+                    style={{marginVertical: 0}}
+                />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default ForgotPasswordCode
